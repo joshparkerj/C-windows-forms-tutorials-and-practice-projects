524099c71de9a674e152cd7f60ce0ee01d5a5ef2
Joshua Parker <10247779@AD.UVU.EDU>
2018-03-30 19:09:38 -0600

    third commit--Matching Game

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        // an integer variable to keep track of matches
+        // once it reaches 8, the player wins
+        // (since there are 8 possible matches in this game.)
+        int score_tracker = 0;
+        Label firstClicked = null;
+        Label secondClicked = null;
+        // once again, we're tryna generate some random thingssss
+        Random randomizer = new Random();
+        // Each of these characters
+        // is some kind of interesting icon
+        // in webdings (each listed 2x for matching purposes)
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            AssignIconsToSquares();
+        }
+        private void AssignIconsToSquares()
+        {
+            foreach(Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = randomizer.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+                return;
+
+            Label clickedLabel = sender as Label;
+            if (clickedLabel != null)
+            {
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+                //clickedLabel.ForeColor = Color.Black;
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+                    return;
+                }
+
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    score_tracker += 1;
+                    if (score_tracker >= 8)
+                    {
+                        MessageBox.Show("Good game, you won!", "congratulations");
+                    }
+                    return;
+                }
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+            firstClicked = null;
+            secondClicked = null;
+
+        }
+    }
+}
